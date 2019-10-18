1a435b8e9abc7e700a11f6fbaabe90dfe8bf8ad1
Sébastien Loriot <sebastien.loriot@cgal.org>
2019-10-18 23:02:18 +0200

    First batch of license header removal
    ack --cpp "MERCHANTABILITY AND FITNESS" -l | xargs perl -i -p0e 's/\/\/ All rights reserved\..*?\$URL\$/\/\/ All rights reserved.\n\/\/\n\/\/ \$URL\$/s'

--- Surface_mesher/archive/demo/Surface_mesher/parameters.cpp
+++ Surface_mesher/archive/demo/Surface_mesher/parameters.cpp
@@ -1,9 +1,6 @@
 // Copyright (c) 2006  INRIA Sophia-Antipolis (France).
 // All rights reserved.
 //
-// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-//
 // $URL$
 // $Id$
 //
