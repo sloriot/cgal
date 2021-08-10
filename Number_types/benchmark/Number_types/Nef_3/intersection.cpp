e710676902a6becd7d024d0a1a6a6f481fe76a90
Dmitry Anisimov <danston@ymail.com>
2021-08-10 14:45:04 +0200

    improved nef 3 bench

--- Number_types/benchmark/Number_types/Nef_3/intersection.cpp
+++ Number_types/benchmark/Number_types/Nef_3/intersection.cpp
@@ -7,6 +7,7 @@
 
 // Kernels.
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 
 // CGAL.
 #include <CGAL/Surface_mesh.h>
@@ -15,47 +16,100 @@
 #include <CGAL/OFF_to_nef_3.h>
 #include <CGAL/Real_timer.h>
 
-using Kernel         = CGAL::Exact_predicates_exact_constructions_kernel;
-using Polygon_mesh   = CGAL::Surface_mesh<typename Kernel::Point_3>;
-using Nef_polyhedron = CGAL::Nef_polyhedron_3<Kernel>;
-using Timer          = CGAL::Real_timer;
+// Boost.
+#include <boost/type_index.hpp>
 
-#define N 10
-int main() {
+using EPECK = CGAL::Exact_predicates_exact_constructions_kernel;
+using EPICK = CGAL::Exact_predicates_inexact_constructions_kernel;
 
-  double total_time = 0.0;
+using Timer = CGAL::Real_timer;
+
+template<typename Kernel>
+double run_bench(
+  const std::string filename1, const std::string filename2,
+  const std::size_t num_iters, const bool verbose) {
+
+  using Point_3        = typename Kernel::Point_3;
+  using Polygon_mesh   = CGAL::Surface_mesh<Point_3>;
+  using Nef_polyhedron = CGAL::Nef_polyhedron_3<Kernel>;
+
+  if (verbose) {
+    std::cout << "* testing kernel: " << boost::typeindex::type_id<Kernel>() << std::endl;
+    std::cout << "- file #1: " << filename1 << std::endl;
+    std::cout << "- file #2: " << filename2 << std::endl;
+    std::cout << "- num iters: " << num_iters << std::endl;
+  }
+
   Polygon_mesh A, B;
+  std::ifstream inA("data/" + filename1);
+  std::ifstream inB("data/" + filename2);
 
-  {
-    std::ifstream in("data/rotated-shifted-spheregrid.off");
-    in >> A;
-  }
+  inA >> A;
+  assert(A.number_of_faces() > 0);
+  assert(A.number_of_vertices() > 0);
 
-  {
-    std::ifstream in("data/rotated-spheregrid.off");
-    in >> B;
-  }
+  inB >> B;
+  assert(B.number_of_faces() > 0);
+  assert(B.number_of_vertices() > 0);
 
   Timer timer;
-  for (int i = 0; i < N; ++i) {
+  double avg_time = 0.0;
+  for (std::size_t k = 0; k < num_iters; ++k) {
     timer.start();
-
-    {
-      Nef_polyhedron nefA(A), nefB(B);
-      Nef_polyhedron nefC = nefA.intersection(nefB);
-      if (i == 0) {
-        std::cout << nefC.number_of_vertices() << "    ";
-      }
-    }
-
-    total_time += timer.time();
+    Nef_polyhedron nefA(A), nefB(B);
+    const Nef_polyhedron nefC = nefA.intersection(nefB);
+    avg_time += timer.time();
     timer.stop();
-    std::cout << timer.time() <<"s, ";
-    std::flush(std::cout);
     timer.reset();
   }
+  avg_time /= static_cast<double>(num_iters);
+  if (verbose) {
+    std::cout << "- avg time: " << avg_time << " sec." << std::endl;
+    std::cout << std::endl;
+  }
+  return avg_time;
+}
 
-  std::cout << "mean time on " << N << " runs: " <<
-    total_time / static_cast<double>(N) << "sec" << std::endl;
+template<typename Kernel>
+void run_all_benches(const std::size_t num_iters, const bool verbose) {
+
+  std::vector<double> times;
+  times.push_back(run_bench<Kernel>("sphere.off", "shifted-spheregrid.off", num_iters, verbose));
+  times.push_back(run_bench<Kernel>("spheregrid.off", "shifted-spheregrid.off", num_iters, verbose));
+  times.push_back(run_bench<Kernel>("spheregrid.off", "sphere.off", num_iters, verbose));
+  times.push_back(run_bench<Kernel>("rotated-shifted-spheregrid.off", "rotated-spheregrid.off", num_iters, verbose));
+
+  if (!verbose) {
+    std::cout << "| N | ";
+    std::cout << "sphere <-> shifted-spheregrid | ";
+    std::cout << "spheregrid <-> shifted-spheregrid | ";
+    std::cout << "spheregrid <-> sphere | ";
+    std::cout << "rotated-shifted-spheregrid <-> rotated-spheregrid | ";
+    std::cout << std::endl;
+    std::cout << "| -- | -- | -- | -- | -- |" << std::endl;
+    std::cout << "| " << num_iters;
+    for (std::size_t k = 0; k < times.size(); ++k) {
+      std::cout << " | " << times[k];
+    }
+    std::cout << " |" << std::endl;
+    std::cout << std::endl;
+  }
+}
+
+int main() {
+
+  std::cout.precision(4);
+  std::cout.setf(std::ios::fixed, std::ios::floatfield);
+
+  std::cout << std::endl;
+  std::cout << " --- NEF_3 BENCH --- " << std::endl;
+  std::cout << std::endl;
+
+  // Parameters.
+  const std::size_t num_iters = 1; // number of iterations to average the timing
+  const bool verbose = false; // do we print extra info
+
+  // Bench.
+  run_all_benches<EPECK>(num_iters, verbose);
   return EXIT_SUCCESS;
 }
