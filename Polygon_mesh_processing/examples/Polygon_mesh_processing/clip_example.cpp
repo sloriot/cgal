0f2c58f0a71ec1194201a45e0ce1c28b7ce7e64e
Sébastien Loriot <sebastien.loriot@cgal.org>
2022-05-17 09:58:47 +0200

    update example for iphigenia

--- Polygon_mesh_processing/examples/Polygon_mesh_processing/clip_example.cpp
+++ Polygon_mesh_processing/examples/Polygon_mesh_processing/clip_example.cpp
@@ -23,26 +23,37 @@ int main(int argc, char* argv[])
     const std::string filename1 = (argc > 1) ? argv[1] : CGAL::data_file_path("meshes/blobby.off");
     const std::string filename2 = (argc > 2) ? argv[2] : CGAL::data_file_path("meshes/eight.off");
     Surface_mesh tet, tri;
-    
+
     if (!PMP::IO::read_polygon_mesh(filename1, tet) || !PMP::IO::read_polygon_mesh(filename2, tri))
     {
         std::cerr << "Invalid input." << std::endl;
         return 1;
     }
 
-  
+  Surface_mesh tet_copy = tet;
 
- // CGAL::make_tetrahedron(Point_3(0,0,0), Point_3(10,0,0), Point_3(0, 10,0), Point_3(0,0,10), tet);
-
-  //CGAL::make_triangle(Point_3(-1, -1, 1), Point_3(11, -1, 1), Point_3(-1, 11,1), tri);
-
+  {
     CGAL::Timer t;
     t.start();
-  PMP::clip(tet, tri);
-  std::cout << t.time() << " sec." << std::endl;
-  std::ofstream out("out.off");
-  out.precision(17);
-  out << tet << std::endl;
+    PMP::clip(tet, tri);
+    std::cout << "New: " << t.time() << " sec." << std::endl;
+    std::ofstream out("new_out.off");
+    out.precision(17);
+    out << tet << std::endl;
+  }
+
+
+  {
+    CGAL::Timer t;
+    t.start();
+    K::Plane_3 p(-0.990461, -0.0105343, 0.137388,169.738);
+    PMP::clip(tet_copy, p);
+    std::cout << "Old: " << t.time() << " sec." << std::endl;
+    std::ofstream out("old_out.off");
+    out.precision(17);
+    out << tet_copy << std::endl;
+  }
+
 
   return EXIT_SUCCESS;
 }
